7cd4b83800f4610d12ffd6ea22a232d237f21155
gleek908i <am_bsk_love_rubio@yahoo.co.jp>
2020-11-27 17:25:26 +0900

    add better button

--- src/LikeButton.jsx
+++ src/LikeButton.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
+
 
 const LikeButton = (props) => {
+    const [count, counter] = useState(0);
+    const [limit, release] = useState(true);
+    const countUp = () => {
+        counter(count+1)
+    }
+
+    useEffect( () => {
+        document.getElementById('counter').addEventListener('click', countUp)
+        if ( count>10 ) {
+            counter(0);
+        }
+        return () => {
+            document.getElementById('counter').removeEventListener('click', countUp)
+        }
+    }, [limit])
+
     return (
-        <button id="counter">いいね数：{props.count}
+        <React.Fragment>
+        <button id="counter">いいね数：{count}
         </button>
+        <button onClick={() => release(!limit)}>もっといいねしたい</button>
+        </React.Fragment>
     )
 }
 
